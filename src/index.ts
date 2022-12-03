a8ba68a39fe0a1977d908d6d6630052492bbba74
rushikesh.patil <rushikesh.patil@infraplan.co.in>
2022-12-03 16:44:16 +0530

    added new cors setting in backend

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ app.use(
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', String(process.env.FRONTEND_URL)],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }
 });
